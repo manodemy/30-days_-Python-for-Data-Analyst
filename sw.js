d271fe0a6a8e2ebc1cbe5eb80acd1723e948173a
Manojkumar <manodamy25@gmail.com>
2026-06-05 02:29:47 +0530

    design: reduce CodeMirror gutters min-width to 26px and bump SW to v24

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'manodemy-static-cache-v23';
-const DYNAMIC_CACHE_NAME = 'manodemy-dynamic-cache-v23';
+const CACHE_NAME = 'manodemy-static-cache-v24';
+const DYNAMIC_CACHE_NAME = 'manodemy-dynamic-cache-v24';
 
 // Static assets to precache immediately
 const PRECACHE_ASSETS = [
