573ff65f4b29b7755a5b035b115727bbc2b71e05
Manojkumar <manodamy25@gmail.com>
2026-06-05 02:12:29 +0530

    design: reduce right side button widths on mobile to prevent scorecard clipping

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'manodemy-static-cache-v20';
-const DYNAMIC_CACHE_NAME = 'manodemy-dynamic-cache-v20';
+const CACHE_NAME = 'manodemy-static-cache-v21';
+const DYNAMIC_CACHE_NAME = 'manodemy-dynamic-cache-v21';
 
 // Static assets to precache immediately
 const PRECACHE_ASSETS = [
